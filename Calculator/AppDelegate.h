cf979eaa2730efb6a4425d8b9b2f88a99d67cb00
The Sanmi <TheSanmi@Sanmis-MacBook-Pro.local>
2015-07-19 19:34:52 +0100

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by The Sanmi on 7/19/15.
+//  Copyright (c) 2015 The Sanmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by The Sanmi on 7/19/15.
+//  Copyright (c) 2015 The Sanmi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
